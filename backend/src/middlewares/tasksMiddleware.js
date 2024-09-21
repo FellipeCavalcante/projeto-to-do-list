a1afb348f0cda2acde9808fa690d5c66ace84d48
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-09-21 20:33:00 -0300

    add put middlewares

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -12,6 +12,21 @@ const validateBody = (req, res, next) => {
   next();
 };
 
+const validateFieldStatus = (req, res, next) => {
+  const { body } = req;
+
+  if (body.status === undefined) {
+    return res.status(400).json({ message: 'The field "status" is required' });
+  }
+
+  if (body.status === '') {
+    return res.status(400).json({ message: 'Status cannot be empty' });
+  }
+
+  next();
+};
+
 module.exports = {
   validateBody,
+  validateFieldStatus
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -7,6 +7,6 @@ const tasksMiddleware = require('./middlewares/tasksMiddleware');
 router.get('/tasks', tasksController.getAll);
 router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
 router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id', tasksController.updateTask);
+router.put('/tasks/:id', tasksMiddleware.validateBody, tasksMiddleware.validateFieldStatus, tasksController.updateTask);
 
 module.exports = router;
